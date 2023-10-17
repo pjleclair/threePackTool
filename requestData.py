a4e496eaaa6d4556f600a4df512e426f039ac5e4
pjleclair <pleclair@protonmail.com>
2023-10-17 12:42:22 -0500

    Implement requestData for Google Sheets

---
+++ requestData.py
@@ -0,0 +1,20 @@
+from google.oauth2.service_account import Credentials
+import googleapiclient.discovery
+
+def main():
+    path = "C:\\Users\\Phil (Rater8)\\Documents\\Scripts\\threePackTool\\credentials.json"
+    credentials = Credentials.from_service_account_file(
+        path,
+        scopes = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+    )
+
+    service = googleapiclient.discovery.build('sheets','v4',credentials=credentials)
+    #id for new client list:
+    spreadsheet_id = '1ZbTHgQc5p61oDLcAHoj2iAWcfgYXJJibnCFMo9boShI'
+    range_ = 'Sheet1'
+    request = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id,range=range_)
+    response = request.execute()
+    print(response)
+
+if __name__ == '__main__':
+    main()
